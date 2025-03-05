5967968ed5646af6f9ab4acd5f482a79022d2819
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-03-05 12:42:43 +0100

    HibernateConfig!!!!

--- src/main/java/app/config/HibernateConfig.java
+++ src/main/java/app/config/HibernateConfig.java
@@ -1,5 +1,7 @@
 package app.config;
 
+import app.entities.Author;
+import app.entities.Poem;
 import app.utils.Utils;
 import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.SessionFactory;
@@ -40,7 +42,8 @@ public class HibernateConfig {
     // TODO: IMPORTANT: Add Entity classes here for them to be registered with Hibernate
     private static void getAnnotationConfiguration(Configuration configuration) {
         //     configuration.addAnnotatedClass(Entity.class);
-
+        configuration.addAnnotatedClass(Poem.class);
+        configuration.addAnnotatedClass(Author.class);
     }
 
     private static EntityManagerFactory createEMF(boolean forTest) {
